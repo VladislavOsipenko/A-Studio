91e093797fd2444493ee0533673139ea6c98c653
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-06-05 20:38:10 +0300

    Header and start main section

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,22 @@ import Swiper, { Navigation, Pagination } from "swiper";
 const swiper = new Swiper('.swiper', {
     
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__li'),
+    hamburger = document.querySelector('.header__hamburger');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('header__hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+    
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+})
+
